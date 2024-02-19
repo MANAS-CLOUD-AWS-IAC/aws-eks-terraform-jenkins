b47fbd26dcc0311f93fdddfe91de6c8035a2d537
Manas Dash <manas.d2@gmail.com>
2024-02-19 22:05:44 +0530

    destroy

--- Modules/VPC/output.tf
+++ Modules/VPC/output.tf
@@ -1,22 +1,27 @@
+# #VPC ID
+# output "vpc_id" {
+#   value = aws_vpc.eks_vpc.id
+# }
+
 #VPC ID
-output "vpc_id" {
-  value = aws_vpc.eks_vpc.id
+output "ec2instance_id" {
+  value = aws_instance.foo.id
 }
 
-#ID of subnet in AZ 1
+# #ID of subnet in AZ 1
 
-output "public_subnet_az1" {
-  value = aws_subnet.public_subnet_az1.id
-}
+# output "public_subnet_az1" {
+#   value = aws_subnet.public_subnet_az1.id
+# }
 
-#ID of subnet in AZ 2
+# #ID of subnet in AZ 2
 
-output "public_subnet_az2" {
-  value = aws_subnet.public_subnet_az2.id
-}
+# output "public_subnet_az2" {
+#   value = aws_subnet.public_subnet_az2.id
+# }
 
-#ID of internet gateway
+# #ID of internet gateway
 
-output "eks_internet_gateway" {
-  value = aws_internet_gateway.eks_internet_gateway.id
-}
+# output "eks_internet_gateway" {
+#   value = aws_internet_gateway.eks_internet_gateway.id
+# }
