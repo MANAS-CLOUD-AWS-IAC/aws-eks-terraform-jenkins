90ccf96e721e54fb4e66804e193d17966703715a
Manas Dash <manas.d2@gmail.com>
2024-02-21 21:46:05 +0530

    create s3 bucket

--- Modules/VPC/main.tf
+++ Modules/VPC/main.tf
@@ -2,10 +2,14 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
+# resource "aws_instance" "foo" {
+#   ami           = "ami-05fa00d4c63e32376" # us-west-2
+#   instance_type = "t2.micro"
+#   tags = {
+#       Name = "TF-Instance"
+#   }
+# }
+
+resource "aws_s3_bucket" "manas_s3_bucket" {
+  bucket = my-tf-test-bucket
 }
